fcd9524fd25c93a0abb2d0088d925207a6a77fe7
Izharulhaq <izharhaq00@gmail.com>
2021-03-27 07:39:54 +0700

    Add volume setting

---
+++ Assets/SetVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void setLevel(float sliderValue)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log(sliderValue) * 30);
+    }
+}
